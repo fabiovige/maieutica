c11d87e27a7228f30b28ffa03f2ba6f6ef5aee1e
Fabio Vige <fabiovige@gmail.com>
2022-09-10 18:21:56 -0300

    corrigido registro de checklist

--- app/Http/Controllers/Api/ChecklistRegisterController.php
+++ app/Http/Controllers/Api/ChecklistRegisterController.php
@@ -14,20 +14,6 @@ use Illuminate\Http\Request;
 
 class ChecklistRegisterController extends Controller
 {
-
-    public function index(Request $request)
-    {
-        $checklist = Checklist::findOrFail($request->checklist_id);
-        $checklist->competences()->sync([
-            44 => ['note' => 4],
-            45 => ['note' => 5],
-            46 => ['note' => 6],
-            47 => ['note' => 7]
-        ]);
-
-        //return ChecklistCompetenceResource::collection($checklist->get());
-    }
-
     public function store(Request $request)
     {
         $arrNotes = explode(',', $request->note);
@@ -37,36 +23,7 @@ class ChecklistRegisterController extends Controller
                 $notes[$c] = ['note' => $v];
             }
         }
-
         $checklist = Checklist::findOrFail($request->checklist_id);
-
-        dd($checklist->competences()->sync($notes));
-
-        dd($notes);
+        $checklist->competences()->syncWithoutDetaching($notes);
     }
-
-    protected function register($checklist_id, $competence_id, $note)
-    {
-        $data = [
-            'checklist_id' => $checklist_id,
-            'competence_id' => $competence_id,
-            'note' => $note
-        ];
-
-        $checklist = Checklist::where('checklist_id', $checklist_id)->where('competence_id', $competence_id);
-        if($checklist->count()) {
-            $checklist->update($data);
-        } else {
-            ChecklistCompetence::create($data);
-        }
-        return true;
-    }
-
-//    public function checklistRegister(Request $request)
-//    {
-//        $checklist_id = $request->checklist_id;
-//        $competence_description_id = $request->competence_description_id;
-//        $c = ChecklistRegister::getChecklistRegister($checklist_id, $competence_description_id);
-//        return ChecklistRegisterResource::collection($c);
-//    }
 }
