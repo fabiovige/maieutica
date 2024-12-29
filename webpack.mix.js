8669883ee4aacb7d6a0777995d08fd56a16a79c0
Fabio Vige <fabiovige@gmail.com>
2024-12-29 09:35:25 -0300

    fix: ajuste nas configurações de build

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const path = require('path');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
@@ -6,11 +7,17 @@ mix.js('resources/js/app.js', 'public/js')
     .copy('resources/images', 'public/images')
     .copy('resources/vendor', 'public/vendor')
     .version()
-    .sourceMaps();
+    .sourceMaps()
+    .webpackConfig({
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/js'),
+            },
+        },
+    });
 
 if (mix.inProduction()) {
     mix.version();
     mix.minify('public/js/app.js');
     mix.minify('public/css/app.css');
 }
-
